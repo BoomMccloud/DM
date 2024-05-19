8d13b9d0ba15969f5521bd995b19ab7e719ba02a
poomchaio <poomchai.ta@gmail.com>
2024-05-19 09:22:12 +0200

    added celebrate and fix layout

--- chrome_extension/pump-overlay/content.js
+++ chrome_extension/pump-overlay/content.js
@@ -1,16 +1,46 @@
-console.log('init');
+
+console.log('init')
 document.body.insertAdjacentHTML(
   'beforeend',
   `<div id="my-overlay">
-    Send your DMs here
-    <form id="my-form">
-      <input type="text" id="my-input">
-      <input type="submit" value="Pump it">
+    <form id="form">
+      <p>Send your DMs here</p>
+      <input id="message" type="text" >
+      <input id="submit-btn" type="submit" value="Pump it">
     </form>
+    <button id="celebrate" style="margin-left: 8px;">celebrate</button>
   </div>`
 );
+document.body.insertAdjacentHTML(
+  'beforeend',
+  `<canvas id="confetti"></canvas>`
+);
 document.querySelector('#my-overlay').style.cssText =
-  'position: fixed; bottom: 0px; left: 0px; right:0; background: white; border: 1px solid black; padding: 10px;z-index: 999999;';
+  `position: fixed;
+  bottom: 0px;
+  gap:8px;
+  left: 0px;
+  right:0;
+  background: white;
+  border: 1px solid black;
+  padding: 10px;
+  z-index: 999999;`
+document.querySelector('#form').style.cssText =
+  `
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  width: 100%;
+  `
+document.querySelector('#message').style.cssText =
+  `
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 2px;
+  flex-grow: 1;
+  `
+document.querySelector('#confetti').style.cssText =
+  'position: absolute; bottom: 0px; left: 0px; right:0; top:0; z-index: 999998;';
 
 // WebSocket connection setup
 const webSocket = new WebSocket('wss://wss.savannah.haus/websocket/');
@@ -30,7 +60,7 @@ webSocket.onmessage = function(event) {
 
     // Extract a specific field from the JSON. Replace 'yourFieldNameHere' with the actual field name
     const fieldValue = jsonData.content;
-    
+
     // Generate a new div element with the field value as its text content
     var newDiv = document.createElement('div');
     newDiv.textContent = fieldValue;
@@ -66,10 +96,11 @@ webSocket.onclose = function(event) {
   console.log("WebSocket connection closed");
 };
 
-document.querySelector('#my-form').addEventListener('submit', function (event) {
+document.querySelector('#form').addEventListener('submit', function (event) {
+
   console.log('test x');
   event.preventDefault();
-  var inputValue = document.querySelector('#my-input').value;
+  var inputValue = document.querySelector('#message').value;
 
   // Prepare the data to send to the server
   const postData = {
@@ -114,4 +145,5 @@ document.querySelector('#my-form').addEventListener('submit', function (event) {
     }
   `;
   document.head.appendChild(style);
-});
+
+  });
